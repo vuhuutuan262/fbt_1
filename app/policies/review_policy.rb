5222dd6e69a96a98c8aecfeb6c53033080360519
ninh.duc.quyen <ninh.duc.quyen@gmail.com>
2016-10-13 10:44:17 +0700

    user show-edit-del review

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,19 @@
+class ReviewPolicy < ApplicationPolicy
+  def update?
+    user.try :is_admin? or is_owner?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.try :is_admin? or is_owner?
+  end
+
+  private
+  def is_owner?
+    user == record.user
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => "/ckeditor"
-  resources :reviews, only: [:new, :create]
+  resources :reviews
   ActiveAdmin.routes self
   get "/static_pages/*page" => "static_pages#show"
   root "static_pages#home"
