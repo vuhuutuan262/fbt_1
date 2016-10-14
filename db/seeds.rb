5270765e8bf4c304525dd696d665dac38cbec6b2
leducphuc <leducphuc1994@gmail.com>
2016-10-14 15:08:26 +0700

    admin manage place

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-User.destroy_all
-
 User.create name: "admin",is_admin: true, email: "admin@admin.com",
   password: "password"
 
@@ -18,4 +9,3 @@ User.create name: "normal", email: "normal@email.com", password: "password"
   password = "password"
   User.create name: name, email: email, password: password
 end
-
