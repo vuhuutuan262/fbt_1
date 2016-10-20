00577152ae677c41f77fd9257a150e57bdcc508e
vuhuutuan262 <vuhuutuan262@gmail.com>
2016-10-20 23:20:38 +0700

    show-place

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@ User.create name: "normal", email: "normal@email.com", password: "password"
   password = "password"
   User.create name: name, email: email, password: password
 end
+
+Place.create name: "Ha Noi", url_image: "hanoi.jpg"
+Place.create name: "Da Nang", url_image: "danang.jpg"
+Place.create name: "Co To", url_image: "coto.jpg"
+Place.create name: "Sa Pa", url_image: "sapa.jpg"
+Place.create name: "Moc Chau", url_image: "mocchau.jpg"
+Place.create name: "Ly Son", url_image: "lyson.jpg"
+Place.create name: "Ha Long", url_image: "halong.jpg"
+Place.create name: "Ba Vi", url_image: "bavi.jpg"
+Place.create name: "Hue", url_image: "hue.jpg"
+Place.create name: "Nha Trang", url_image: "nhatrang.jpg"
